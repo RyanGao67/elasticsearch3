9476a40e8ac60b5cb8f18a21b9195c825f79986e
RyanGao67 <tiangao0611@gmail.com>
2021-01-03 14:26:25 -0500

    a

--- src/main/java/com/shallowlightning/App.java
+++ src/main/java/com/shallowlightning/App.java
@@ -1,5 +1,4 @@
 package com.shallowlightning;
-
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -33,18 +32,35 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
-
 public class App {
     private static RestHighLevelClient client = SingletonClient.getClient();
     public static void main(String[] args) throws URISyntaxException, IOException {
 //        deleteIndex();
 //        createTemplate();
 //        indexIndex();
-//        catShards();
-//        catNodes();
-        allocateReplica();
-        allocatePrimary();
+        checkAllocate();
         client.close();
+    }
+
+    // for testing you may need this
+    // curl -XPUT localhost:9200/_cluster/settings -H 'Content-Type:application/json' -d \
+    // '{"persistent" : {"cluster.routing.allocation.enable":"primaries"}}'
+    // reference: https://www.elastic.co/guide/en/elasticsearch/reference/current/cluster-reroute.html
+    // reference: https://www.elastic.co/guide/en/elasticsearch/reference/current/cluster-update-settings.html
+    public static void checkAllocate() throws IOException {
+        System.out.println("nodes: ");
+        ArrayNode nodes = catNodes();   nodes.forEach(node->System.out.println(node));
+        System.out.println("shards: ");
+        ArrayNode shards = catShards();     shards.forEach(shard->System.out.println(shard));
+        shards.forEach(shard -> {
+            if(shard.get("state").textValue().equals("UNASSIGNED") && shard.get("prirep").textValue().equals("r")){
+                try {allocateReplica(shard.get("index").textValue(), shard.get("shard").intValue(), "172.19.0.4" );}
+                catch (IOException e) {    e.printStackTrace();                }
+            }else if(shard.get("state").textValue().equals("UNASSIGNED") && shard.get("prirep").textValue().equals("r")){
+                try                   {    allocatePrimary();                }
+                catch (IOException e) {    e.printStackTrace();              }
+            }
+        });
     }
 
     public static boolean createTemplate() throws URISyntaxException {
@@ -69,7 +85,6 @@ public class App {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         // Step 2: send the request
         request.source(template, XContentType.JSON);
         // In case multiple templates match an index, the orders of matching templates determine the sequence
@@ -80,7 +95,6 @@ public class App {
         // by external systems.
         request.version(4);
         request.masterNodeTimeout("1m");
-
         try{
             AcknowledgedResponse putTemplateResponse = client.indices().putTemplate(request, RequestOptions.DEFAULT);
             if(putTemplateResponse.isAcknowledged()){
@@ -118,10 +132,12 @@ public class App {
         }catch (IOException e){
             System.out.println(e.toString());
         }
+/////////////////////////////////////////Another approach
 //        String doc = IOUtils.toString(
 //                App.class.getResourceAsStream(String.format("/%s.json", document)),
 //                StandardCharsets.UTF_8
 //        );
+/////////////////////////////////////////
         System.out.println(doc);
         JsonNode jsonDocuments = mapper.readTree(doc);
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -166,7 +182,7 @@ public class App {
         }
     }
 
-    public static void catShards() throws IOException {
+    public static ArrayNode catShards() throws IOException {
         String endpoint = String.format("/_cat/shards?format=json");
         Request request = new Request("GET", endpoint);
         Response response = client.getLowLevelClient().performRequest(request);
@@ -174,21 +190,10 @@ public class App {
         ArrayNode shards = (ArrayNode) new ObjectMapper()
                 .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
                 .readTree(result);
-        System.out.println(shards);
-//        {"index":"risk_scores","shard":"0","prirep":"p","state":"STARTED","docs":"55","store":"24.7kb","ip":"172.19.0.3","node":"es01"}
-        shards.forEach(shard -> {
-            System.out.println("index: "+shard.get("index"));
-            System.out.println("shard: "+shard.get("shard"));
-            System.out.println("prirep: "+shard.get("prirep"));
-            System.out.println("state: "+shard.get("state"));
-            System.out.println("docs: "+shard.get("docs"));
-            System.out.println("store: "+shard.get("store"));
-            System.out.println("ip: "+shard.get("ip"));
-            System.out.println("node: "+shard.get("node"));System.out.println();
-        });
+        return shards;
     }
 
-    public static void catNodes() throws IOException {
+    public static ArrayNode catNodes() throws IOException {
         String endpoint = String.format("/_cat/nodes?format=json");
         Request request = new Request("GET", endpoint);
         Response response = client.getLowLevelClient().performRequest(request);
@@ -196,26 +201,12 @@ public class App {
         ArrayNode nodes = (ArrayNode) new ObjectMapper()
                 .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
                 .readTree(result);
-        System.out.println(nodes);
-//        {"ip":"172.19.0.4","heap.percent":"67","ram.percent":"98","cpu":"19","load_1m":"1.47","load_5m":"2.09","load_15m":"4.04","node.role":"cdhilmrstw","master":"-","name":"es03"},
-        nodes.forEach(shard -> {
-            System.out.println("ip: "+shard.get("ip"));
-            System.out.println("heap.percent: "+shard.get("heap.percent"));
-            System.out.println("ram.percent: "+shard.get("ram.percent"));
-            System.out.println("cpu: "+shard.get("cpu"));
-            System.out.println("load_1m: "+shard.get("load_1m"));
-            System.out.println("load_5m: "+shard.get("load_5m"));
-            System.out.println("load_15m: "+shard.get("load_15m"));
-            System.out.println("node.role: "+shard.get("node.role"));
-            System.out.println("master: "+shard.get("master"));
-            System.out.println("name: "+shard.get("name"));
-            System.out.println();
-        });
+        return nodes;
     }
 
     //curl --silent -XPOST "localhost:9200/_cluster/reroute" -H "Content-Type:application/json"
     // -d '{"commands":[{"allocate_replica":{"index":"risk_scores", "shard":0, "node":"172.19.0.4"}}]}'
-    public static void allocateReplica() throws IOException {
+    public static void allocateReplica(String index, int shard, String target) throws IOException {
         try {
             Request request = new Request(
                     "POST",
@@ -223,9 +214,9 @@ public class App {
             request.setJsonEntity(
                 String.format(
                     "{\"commands\":[{\"allocate_replica\":{\"index\":\"%s\", \"shard\":%s, \"node\":\"%s\"}}]}",
-                    "risk_scores",
-                    0,
-                    "172.19.0.4"
+                    index,
+                    shard,
+                    target
                 )
             );
             Response response = client.getLowLevelClient().performRequest(request);
@@ -254,7 +245,6 @@ public class App {
             System.out.println(e.toString());
         }
     }
-
 
     public static boolean deleteIndex() throws IOException {
         DeleteIndexRequest request = new DeleteIndexRequest("risk_scores");
